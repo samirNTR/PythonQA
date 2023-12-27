021f73fe49b439e0c2bba3003bb79bbc1529951d
samir <samirviral846@gmail.com>
2023-12-27 22:43:56 +0530

    new commit

--- eclipse-workspace/MyPython/MyPractice/Strings.py
+++ eclipse-workspace/MyPython/MyPractice/Strings.py
@@ -1,12 +1,18 @@
-'''
-Created on 27-Dec-2023
+import string
 
-@author: samir
-'''
 #Both quotes are accepted
 x="I love python"
 y='I love python'
 
+
+a="I  'love' python very much"
+
+b='I  "love" python very much'
+c="I  \'love\' python very much"  #Escape sequence
+
+print("This is " +a)
+print(b)
+print(c)
 print(x)
 print(y)
 
@@ -14,3 +20,19 @@ print(y)
 
 print(x[2])
 print(y[9])
+
+#Using strings to define multiline comments
+
+z="""
+This is multiline string                '
+"""
+
+c='''
+This is also a multiline string
+'''
+
+print(z)
+print(c)
+#Membership operator in string
+
+print("iss" in c)
